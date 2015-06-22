208651b20490b9b006eca10baf52bd231f45278b
Anton Staroverov <by@tonystar.me>
2015-06-22 20:38:02 +0300

    Support external links as tabs

--- bootstrap-hover-tabs.js
+++ bootstrap-hover-tabs.js
@@ -1,6 +1,7 @@
 (function ($) {
   $(function () {
-    $(document).on('mouseenter', '[data-toggle="tab"]', function () {
+    $(document).off('click.bs.tab.data-api', '[data-hover="tab"]');
+    $(document).on('mouseenter.bs.tab.data-api', '[data-toggle="tab"], [data-hover="tab"]', function () {
       $(this).tab('show');
     });
   });
